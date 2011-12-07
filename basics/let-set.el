31723b28e2e091bec5c0ac33ef54301e02eb370c
Jan Gloser <jan.gloser@centrum.cz>
2011-12-07 17:42:19 +0100

    Assigning values to variables

---
+++ basics/let-set.el
@@ -0,0 +1,12 @@
+(set 'colours '(red orange yellow green blue violet pink))
+colours
+
+; I don't have to quote the variable with setq
+(setq one 1)
+(setq la_lista '(1 2 3))
+
+; multiple assign
+(setq en 'english
+      de 'deutsch)
+
+; let is probably something like a local variable binding, more on that later
